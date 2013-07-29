7ab4be79f2547e4bfc10fd6332014358ee8c53fb
oleger814 <jandrewl814@yahoo.com>
2013-07-29 13:20:30 -0700

    Pre-last models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Photos::Application.routes.draw do
 
   match 'photos', :to => "photos#index"
 
-  match 'slideshow', :to => "photos#slideshow"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
